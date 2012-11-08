ade8d22c4cddce7c154cfebcab14b64e9cca7cc4
IrisMa <mistmilo@gmail.com>
2012-11-08 12:54:45 +0800

    add new gem annotate in Gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,28 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
-  has_many :posts
 
-  def initialize(attributes = {})
-    @name  = attributes[:name]
-    @email = attributes[:email]
-  end
+  attr_accessible :name, :email
 
-  def formatted_email
-    "#{@name} <#{@email}>"
-  end
+  #attr_accessible :email, :name
+  #has_many :posts
+  #
+  #def initialize(attributes = {})
+  #  @name  = attributes[:name]
+  #  @email = attributes[:email]
+  #end
+  #
+  #def formatted_email
+  #  "#{@name} <#{@email}>"
+  #end
 
 end
