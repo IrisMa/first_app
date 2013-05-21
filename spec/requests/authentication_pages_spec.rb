4e4c59b2fa15077b089f52ecc162275962c13181
IrisMa <mistmilo@gmail.com>
2013-05-22 00:00:50 +0800

    updated application and sessiong controller, update user model.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -21,4 +21,23 @@ describe "Authentication" do
       it { should_not have_link ('Sign in', href: signin_path) }
     end
   end
+
+
+  describe "signin" do
+
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_selector('title', text: 'Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+        end
+    end
+  end
+
 end
